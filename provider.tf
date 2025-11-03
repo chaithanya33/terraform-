fb3d67f5de0c91fb254a583cb057563d9b844b6f
chaitu <chaituchaithanya200@gmail.com>
2025-11-03 09:48:32 +0530

    3rd commit

--- provider.tf
+++ provider.tf
@@ -1,3 +1,4 @@
+
 #this is a basic one to create instance 
 
 provider "aws" {
@@ -11,4 +12,12 @@ Name = "terra-server"
 
 ami = " "
 instance_type = t2.micro"
+# for launch two instances at time
+count = 2
 }
+
+output "abc" {
+  value = [aws_instance.label33"[0].public_ip, aws_instance.flm[0].privat_ip] 
+
+  #label name of server# to see the particular server details like pucli,private
+
