233e0b5dde28f78de2c295f94431298540a248a3
gopineedi chaithanya <chaithanyagopineedi@gmail.com>
2025-11-05 12:20:25 +0530

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -3,7 +3,7 @@ name = "terraform-lb"
 subnets = [aws_subnet.two.id, aws_subnet.three.id] # to attach subnets here
 listener {
 instance_port = 80 #where the application running on port we should give port number
-instance_protocol = "httpd"
+instance_protocol = "http"
 lb_port = 80
 lb_protocol = "http"
 }
